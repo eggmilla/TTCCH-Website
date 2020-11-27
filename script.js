c2b58a6ff1ca42944363e021961225365f73192b
milla kim <millakimm1@gmail.com>
2020-11-27 04:02:29 +0000

    haven't committed in a long time

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementsByClassName("navB");
-
-// Get the offset position of the navbar
-var sticky = navB.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navB.classList.add("sticky")
+/* FAQ click to reveal text */
+/* https://stackoverflow.com/questions/55442477/make-text-appear-disappear-on-button-click */
+function toggleText() {
+  var text = document.getElementById("demo");
+  if (text.style.display === "none") {
+    text.style.display = "block";
   } else {
-    navB.classList.remove("sticky");
+    text.style.display = "none";
   }
 }
+
